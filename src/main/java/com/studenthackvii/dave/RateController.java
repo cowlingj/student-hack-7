3bc0e32661f934134938b8c2e130cfda7c79dd3c
Sophie <sophiephan364@gmail.com>
2019-03-30 16:47:24 +0000

    Simple form

--- src/main/java/com/studenthackvii/dave/RateController.java
+++ src/main/java/com/studenthackvii/dave/RateController.java
@@ -5,10 +5,9 @@ import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
 
 @Controller
 public class RateController {
@@ -17,16 +16,16 @@ public class RateController {
     private Resource comicList;
 
   @GetMapping("/rate")
-  public String getCartoons() {
+  public String getCartoons(Model model) {
     Logger
       .getAnonymousLogger()
       .info(String.format("resource exists: %s", comicList.exists()));
+    model.addAttribute("ratings", new Rating());
     return "/test.html";
   }
 
     @PostMapping("/rate")
-    public String postRatings( @ModelAttribute("ratings") Rating[] ratings) {
-
+    public String postRatings(@ModelAttribute("ratings") Rating ratings, BindingResult errors) {
 
         return "/index.html";
     }
--- src/main/java/com/studenthackvii/dave/Rating.java
+++ src/main/java/com/studenthackvii/dave/Rating.java
@@ -1,14 +1,9 @@
 package com.studenthackvii.dave;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class Rating {
     private int id;
     private int review;
 
-    public Rating() {
-    }
 
     public void setId(int id) {
         this.id = id;
