7ee4bef46921fdf42eaf6db7484131773dcf02e5
Unknown <sophiephan364@gmail.com>
2019-03-30 19:39:46 +0000

    gets the 10 cartoons

--- src/main/java/com/studenthackvii/dave/RateController.java
+++ src/main/java/com/studenthackvii/dave/RateController.java
@@ -14,6 +14,7 @@ import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
