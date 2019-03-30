e0b47be4c29ed7ccd2a570f41b888ce0d6b9c929
Jonathan Cowling <09cowlingj@gmail.com>
2019-03-30 14:01:31 +0000

    Basic controller stuff

--- src/main/java/com/studenthackvii/dave/DaveApplication.java
+++ src/main/java/com/studenthackvii/dave/DaveApplication.java
@@ -2,12 +2,27 @@ package com.studenthackvii.dave;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class DaveApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DaveApplication.class, args);
+	}
+
+}
+
+@Controller
+class C {
+
+	@RequestMapping("/")
+	public String hellowWorld(Model m) {
+
+		m.addAttribute("text", "hello world");
+		return "index.html";
 	}
 
 }
