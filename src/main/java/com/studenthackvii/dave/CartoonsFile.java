da15fd0d93c833d5c95d00acd26cac92b7ec098b
Unknown <sophiephan364@gmail.com>
2019-03-30 19:35:19 +0000

    can get the size of the cartoons thing

--- src/main/java/com/studenthackvii/dave/CartoonsFile.java
+++ src/main/java/com/studenthackvii/dave/CartoonsFile.java
@@ -1,30 +1,87 @@
 package com.studenthackvii.dave;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
-@Component
-@PropertySource(value = "/peter.json")
-@ConfigurationProperties
 public class CartoonsFile {
-  private CartoonItem[] cartoons;
+  private List<CartoonItem> cartoons = new ArrayList<>();
 
-  public CartoonItem[] getCartoons()
+  public List<CartoonItem> getCartoons()
   {
       return cartoons;
   }
 
-  public void setCartoons(CartoonItem[] cartoons)
+  public void setCartoons(List<CartoonItem> cartoons)
   {
       this.cartoons = cartoons;
   }
 
-  @Component
-  public class CartoonItem {
+  public static class CartoonItem {
       private String name;
-      private String url;
-      private int id;
-      private String[] genre;
+        private String url;
+        private int id;
+        private String[] genre;
+
+        /**
+         * @return the name
+         */
+        public String getName() {
+            return name;
+        }
+
+        /**
+         * @return the genre
+         */
+        public String[] getGenre() {
+            return genre;
+        }
+
+        /**
+         * @param genre the genre to set
+         */
+        public void setGenre(String[] genre) {
+            this.genre = genre;
+        }
+
+        /**
+         * @return the id
+         */
+        public int getId() {
+            return id;
+        }
+
+        /**
+         * @param id the id to set
+         */
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        /**
+         * @return the url
+         */
+        public String getUrl() {
+            return url;
+        }
+
+        /**
+         * @param url the url to set
+         */
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        /**
+         * @param name the name to set
+         */
+        public void setName(String name) {
+            this.name = name;
+        }
+
+
   }
 }
--- src/main/java/com/studenthackvii/dave/RateController.java
+++ src/main/java/com/studenthackvii/dave/RateController.java
@@ -1,7 +1,13 @@
 package com.studenthackvii.dave;
 
+import java.io.IOException;
 import java.util.logging.Logger;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
@@ -12,14 +18,19 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class RateController {
 
-    @Value("classpath:/peter.json")
-    private Resource comicList;
+  private ObjectMapper mapper = new ObjectMapper();
+
+  @Value("classpath:/peter.json")
+  private Resource cartoonsList;
 
   @GetMapping("/rate")
-  public String getCartoons(Model model) {
+  public String getCartoons(Model model) throws JsonParseException, JsonMappingException, IOException {
+
+    CartoonsFile cartoonsFile = mapper.readValue(cartoonsList.getFile(), CartoonsFile.class);
+
     Logger
       .getAnonymousLogger()
-      .info(String.format("resource exists: %s", comicList.exists()));
+      .info(String.format("cartoonsFile items: %d", cartoonsFile.getCartoons().size()));
     model.addAttribute("ratings", new Rating());
     return "/test.html";
   }
