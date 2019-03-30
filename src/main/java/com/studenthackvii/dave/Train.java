8090705d671f44880cad9fda9df930dc684d0661
Jonathan Cowling <09cowlingj@gmail.com>
2019-03-30 23:44:42 +0000

    even more tf :)

--- src/main/java/com/studenthackvii/dave/Train.java
+++ src/main/java/com/studenthackvii/dave/Train.java
@@ -4,13 +4,7 @@ import java.nio.FloatBuffer;
 import java.util.logging.Logger;
 
 import org.springframework.stereotype.Component;
-import org.tensorflow.DataType;
-import org.tensorflow.Graph;
-import org.tensorflow.Operation;
-import org.tensorflow.SavedModelBundle;
-import org.tensorflow.Session;
-import org.tensorflow.Tensor;
-import org.tensorflow.TensorFlow;
+import org.tensorflow.*;
 
 @Component
 public class Train {
@@ -21,64 +15,59 @@ public class Train {
         // vars
         Operation ratingsMulVar = g.opBuilder("Variable", "ratingsVm")
                 .setAttr("dtype", DataType.FLOAT)
-                .setAttr("shape", new long[]{10})
+                .setAttr("shape", Shape.make(10))
                 .build();
 
         Operation confidenceMulVar = g.opBuilder("Variable", "confidenceVm")
                 .setAttr("dtype", DataType.FLOAT)
-                .setAttr("shape", new long[]{10})
+                .setAttr("shape", Shape.make(10))
                 .build();
 
         Operation ratingsAddVar = g.opBuilder("Variable", "ratingsVc")
                 .setAttr("dtype", DataType.FLOAT)
-                .setAttr("shape", new long[]{10})
+                .setAttr("shape", Shape.make(10))
                 .build();
 
         Operation confidenceAddVar = g.opBuilder("Variable", "confidenceVc")
                 .setAttr("dtype", DataType.FLOAT)
-                .setAttr("shape", new long[]{10})
+                .setAttr("shape", Shape.make(10))
                 .build();
 
         // inputs
         Operation ratingsInput = g.opBuilder("Placeholder", "ratingsIn")
                 .setAttr("dtype", DataType.FLOAT)
-                .setAttr("shape", new long[]{10})
+                .setAttr("shape", Shape.make(10))
                 .build();
 
         Operation confidenceInput = g.opBuilder("Placeholder", "confidenceIn").
                 setAttr("dtype", DataType.FLOAT)
-                .setAttr("shape", new long[]{10})
+                .setAttr("shape", Shape.make(10))
                 .build();
 
         // m x
         Operation ratTimesM = g.opBuilder("Mul", "ratingM")
-                .setAttr("dtype", DataType.FLOAT)
                 .addInput(ratingsInput.output(0))
                 .addInput(ratingsMulVar.output(0))
                 .build();
 
         Operation conTimesM = g.opBuilder("Mul", "confidenceM")
-                .setAttr("dtype", DataType.FLOAT)
                 .addInput(confidenceInput.output(0))
                 .addInput(confidenceMulVar.output(0))
                 .build();
 
         // m x + c
         Operation ratMPlusC = g.opBuilder("Add", "ratingMc")
-                .setAttr("dtype", DataType.FLOAT)
                 .addInput(ratTimesM.output(0))
                 .addInput(ratingsAddVar.output(0))
                 .build();
 
         Operation conMPlusC = g.opBuilder("Add", "confidenceMc")
-                .setAttr("dtype", DataType.FLOAT)
                 .addInput(conTimesM.output(0))
                 .addInput(confidenceAddVar.output(0))
                 .build();
 
 
         Operation result = g.opBuilder("Mul", "res")
-                .setAttr("dtype", DataType.FLOAT)
                 .addInput(ratMPlusC.output(0))
                 .addInput(conMPlusC.output(0))
                 .build();
@@ -86,6 +75,9 @@ public class Train {
         Tensor<Float> ratings = Tensor.create(new long[]{10}, FloatBuffer.allocate(10));
         Tensor<Float> confidence = Tensor.create(new long[]{10}, FloatBuffer.allocate(10));
 
+        g.opBuilder("Assign", "assign 1");
+
+        
         Tensor<Float> tensor = (Tensor<Float>) new Session(g)
                 .runner()
                 .feed("ratingsIn", ratings)
