7bd1110a21f76d47d319e4474845244b46177c8f
Jonathan Cowling <09cowlingj@gmail.com>
2019-03-30 21:35:30 +0000

    Add tf initial stuff

--- src/main/java/com/studenthackvii/dave/Train.java
+++ src/main/java/com/studenthackvii/dave/Train.java
@@ -3,6 +3,7 @@ package com.studenthackvii.dave;
 import java.nio.FloatBuffer;
 
 import org.tensorflow.Graph;
+import org.tensorflow.Operation;
 import org.tensorflow.SavedModelBundle;
 import org.tensorflow.Session;
 import org.tensorflow.Tensor;
@@ -12,7 +13,17 @@ public class Train {
 
   public void name() {
     Graph g = new Graph();
-    Session s = new Session(g);
-    
+    Tensor<Float> ratings = Tensor.create(new long[]{10}, FloatBuffer.allocate(10));
+    Tensor<Float> confidence = Tensor.create(new long[]{10}, FloatBuffer.allocate(10));
+
+    Operation ratingsOp = g.opBuilder("Variable", "ratings")
+      .setAttr("dtype", ratings.dataType())
+      .setAttr("value", ratings)
+      .build();
+
+    Operation confidenceOp = g.opBuilder("Variable", "confidence")
+      .setAttr("dtype", confidence.dataType())
+      .setAttr("value", confidence)
+      .build();
   }
 }
