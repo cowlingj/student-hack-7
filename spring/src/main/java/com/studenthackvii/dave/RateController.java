62a8d634e7863a0c15f880aefcc4b4a3853b317d
Unknown <sophiephan364@gmail.com>
2019-03-31 11:00:30 +0100

    lerp

--- spring/src/main/java/com/studenthackvii/dave/RateController.java
+++ spring/src/main/java/com/studenthackvii/dave/RateController.java
@@ -74,47 +74,17 @@ public class RateController {
         return "/recommended.html";
     }
 
-    private void peter() {
-        try (Graph g = new Graph()) {
-            final String value = "Hello from " + TensorFlow.version();
+    public int lerp(int value, int a1, int a2, int b1, int b2) {
 
-            // Construct the computation graph with a single operation, a constant
-            // named "MyConst" with a value "value".
-            try (Tensor t = Tensor.create(value.getBytes(StandardCharsets.UTF_8))) {
-                // The Java API doesn't yet include convenience functions for adding operations.
-                g.opBuilder("Const", "MyConst").setAttr("dtype", t.dataType()).setAttr("value", t).build();
-            }
-
-            try (Session s = new Session(g); Tensor output = s.runner().fetch("MyConst").run().get(0)) {
-                System.out.println(new String(output.bytesValue(), StandardCharsets.UTF_8));
-            }
+        if (value <= a1) {
+            return b1;
         }
-    }
 
-    private double[] converter(Rating rating, int occurrences) {
+        if (value >= a2) {
+            return b2;
+        }
 
-        final int GET_HALF = 2, ARRAY_SIZE = 2, SET_RATING = 0, SET_OCCURRENCES = 1, LOWER_DOWN_NUMBER = 3;
-        final int MAXIMUM_OCCURRENCES = 99, MAXIMUM_RATING = 5, MINIMUM_RATING = 1, MINIMUM_OCCURRENCES = 0;
-        final double CONVERT_TO_DECIMAL = 0.01;
-
-        if (rating.getId() > MAXIMUM_RATING)
-            rating.setId(MAXIMUM_RATING);
-        if (rating.getId() < MINIMUM_RATING)
-            rating.setId(MINIMUM_RATING);
-
-        if (occurrences < MINIMUM_OCCURRENCES)
-            occurrences = MINIMUM_OCCURRENCES;
-        if (occurrences > MAXIMUM_OCCURRENCES)
-            occurrences = MAXIMUM_OCCURRENCES;
-
-        final int newRating = rating.getReview() - LOWER_DOWN_NUMBER / GET_HALF; // formula;
-        final double newOccurrences = occurrences * CONVERT_TO_DECIMAL;
-
-        double[] array = new double[ARRAY_SIZE];
-        array[SET_RATING] = newRating;
-        array[SET_OCCURRENCES] = newOccurrences;
-
-        return array;
+        return (int)(((double)(value - a1)) / (a2 - a1) * (b2 - b1) ) + b1;
     }
 
 }
