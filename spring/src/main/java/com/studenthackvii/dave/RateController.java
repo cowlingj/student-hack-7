ba992bc538de7be5befa29f9796653a8dd7bc509
Unknown <sophiephan364@gmail.com>
2019-03-31 10:41:31 +0100

    ratings objects

--- spring/src/main/java/com/studenthackvii/dave/RateController.java
+++ spring/src/main/java/com/studenthackvii/dave/RateController.java
@@ -4,8 +4,11 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.FloatBuffer;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.logging.Logger;
 import java.util.stream.IntStream;
+
+import javax.annotation.PostConstruct;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -17,12 +20,14 @@ import org.json.simple.parser.JSONParser;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.tensorflow.*;
 
 @Controller
@@ -33,46 +38,39 @@ public class RateController {
     @Value("classpath:/data/cartoonsToRate.json")
     private Resource cartoonsList;
 
-    @GetMapping("/rate")
-    public String getCartoons(Model model) throws JsonParseException, JsonMappingException, IOException {
-        CartoonsFile cartoonsFile = mapper.readValue(cartoonsList.getFile(), CartoonsFile.class);
+    CartoonsFile cartoonsFile;
 
-        Logger
-                .getAnonymousLogger()
-                .info(String.format("cartoonsFile items: %d", cartoonsFile.getCartoons().size()));
+    @PostConstruct
+    private void init() throws JsonParseException, JsonMappingException, IOException {
+        cartoonsFile = mapper.readValue(cartoonsList.getFile(), CartoonsFile.class);
+    }
+
+    @GetMapping("/rate")
+    public String getCartoons(Model model) {
+
+        Logger.getAnonymousLogger().info(String.format("cartoonsFile items: %d", cartoonsFile.getCartoons().size()));
         model.addAttribute("ratings", new Rating());
         model.addAttribute("cartoons", cartoonsFile.getCartoons());
         return "/cartoon-list.html";
     }
 
-    @PostMapping("/rate")
-    public String postRatings(@ModelAttribute("ratings") Rating ratings) {
-        JSONParser parser = new JSONParser();
+    @PostMapping(value = "/rate", consumes = { MediaType.APPLICATION_FORM_URLENCODED_VALUE })
+    public String postRatings(@RequestParam int[] ids, @RequestParam int[] reviews) {
 
-        try {
-            final String FILE_PATH = "C:\\Users\\Peter Boncheff\\IntelliJIDEAProjects\\student-hack-7\\src\\main\\resources\\data\\cartoonsToRate.json" ;
-            final String JSON_NAME = "name", JSON_URL = "url", JSON_ID = "id", JSON_GENRE = "genre";
+        Rating[] ratingArray = new Rating[ids.length];
+        Rating currentRating;
 
-            Object obj = parser.parse(new FileReader(FILE_PATH));
+        for (int i = 0; i < ratingArray.length; i++) {
+            currentRating = new Rating();
+            currentRating.setId(ids[i]);
+            currentRating.setReview(reviews[i]);
+            ratingArray[i] = currentRating;
 
-            JSONObject jsonObject = (JSONObject) obj;
-            System.out.println(jsonObject);
-
-            String name = (String) jsonObject.get(JSON_NAME);
-            int id = (Integer) jsonObject.get(JSON_ID);
-
-            String url = (String) jsonObject.get(JSON_URL);
-            JSONArray genre = (JSONArray) jsonObject.get(JSON_GENRE);
-            IntStream.range(0, genre.size())
-                    .mapToObj(genre::get)
-                    .forEach(System.out::println);
-
-
-
-        } catch (IOException | org.json.simple.parser.ParseException e) {
-            e.printStackTrace();
+            // checking the Rating objects have the ID and Review
+            Logger.getAnonymousLogger().info(String.format("rate id: %d", ratingArray[i].getId()));
+            Logger.getAnonymousLogger().info(String.format("rate review: %d", ratingArray[i].getReview()));
         }
-        peter();
+
         return "/recommended.html";
     }
 
@@ -87,8 +85,7 @@ public class RateController {
                 g.opBuilder("Const", "MyConst").setAttr("dtype", t.dataType()).setAttr("value", t).build();
             }
 
-            try (Session s = new Session(g);
-                 Tensor output = s.runner().fetch("MyConst").run().get(0)) {
+            try (Session s = new Session(g); Tensor output = s.runner().fetch("MyConst").run().get(0)) {
                 System.out.println(new String(output.bytesValue(), StandardCharsets.UTF_8));
             }
         }
@@ -100,11 +97,15 @@ public class RateController {
         final int MAXIMUM_OCCURRENCES = 99, MAXIMUM_RATING = 5, MINIMUM_RATING = 1, MINIMUM_OCCURRENCES = 0;
         final double CONVERT_TO_DECIMAL = 0.01;
 
-        if (rating.getId() > MAXIMUM_RATING) rating.setId(MAXIMUM_RATING);
-        if (rating.getId() < MINIMUM_RATING) rating.setId(MINIMUM_RATING);
+        if (rating.getId() > MAXIMUM_RATING)
+            rating.setId(MAXIMUM_RATING);
+        if (rating.getId() < MINIMUM_RATING)
+            rating.setId(MINIMUM_RATING);
 
-        if (occurrences < MINIMUM_OCCURRENCES) occurrences = MINIMUM_OCCURRENCES;
-        if (occurrences > MAXIMUM_OCCURRENCES) occurrences = MAXIMUM_OCCURRENCES;
+        if (occurrences < MINIMUM_OCCURRENCES)
+            occurrences = MINIMUM_OCCURRENCES;
+        if (occurrences > MAXIMUM_OCCURRENCES)
+            occurrences = MAXIMUM_OCCURRENCES;
 
         final int newRating = rating.getReview() - LOWER_DOWN_NUMBER / GET_HALF; // formula;
         final double newOccurrences = occurrences * CONVERT_TO_DECIMAL;
